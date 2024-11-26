527859f088be61644306b1bd5eb501ed43f7fdae
Josh Long <josh@joshlong.com>
2024-11-26 12:02:28 -0800

    adding another example

--- framework/src/main/java/bootiful/framework/HtmxController.java
+++ framework/src/main/java/bootiful/framework/HtmxController.java
@@ -12,10 +12,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.FragmentsRendering;
 
 import java.time.Instant;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 
@@ -67,13 +69,28 @@ class TodosController {
         return "todos :: todos";
     }
 
+/*
     @PostMapping
-    Collection<ModelAndView> add(@RequestParam("new-todo") String newTodo, Model model) {
+    FragmentsRendering addFragmentsRendering(@RequestParam("new-todo") String newTodo) {
         this.repository.save(new Todo(null, newTodo));
-        model.addAttribute("todos", this.repository.findAll());
-        return List.of(new ModelAndView("todos :: todos"),
-                new ModelAndView("todos :: todos-form"));
+        var model = Map.of("todos", (Object) this.repository.findAll());
+        return FragmentsRendering
+                .with("todos :: todos")
+                .fragment("todos :: todos-form", model)
+                .headers(httpHeaders -> httpHeaders.setAccessControlMaxAge(100))
+                .build();
     }
+*/
+
+    @PostMapping
+    Collection<ModelAndView> addModelAndView(@RequestParam("new-todo") String newTodo) {
+        this.repository.save(new Todo(null, newTodo));
+        var todos = new ModelAndView("todos :: todos");
+        var model = Map.of("todos", this.repository.findAll());
+        var todosForm = new ModelAndView("todos :: todos-form", model);
+        return List.of(todos, todosForm);
+    }
+
 
     @ResponseBody
     @DeleteMapping(path = "/{todoId}", produces = MediaType.TEXT_HTML_VALUE)
